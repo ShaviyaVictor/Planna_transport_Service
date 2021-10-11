cae533fd1dd2e9a5eac73b02f6e9e7b2605471b5
james-muriithi <muriithijames556@gmail.com>
2021-10-11 12:15:11 +0300

    added trip objects

--- js/home.js
+++ js/home.js
@@ -38,70 +38,25 @@ const buses = [
 ];
 
 const trips = [
-  {
-    id: 1,
-    bus_id: 1,
-    route_id: 2,
-    fare: 200,
-    currency: "Ksh",
-    pickup_time: "16:00",
-    travel_date: new Date(),
-    booked_seats: 0,
-  },
-  {
-    id: 2,
-    bus_id: 2,
-    route_id: 1,
-    fare: 100,
-    currency: "Ksh",
-    pickup_time: "14:00",
-    travel_date: new Date(),
-    booked_seats: 2,
-  },
+  new Trip(1, 1, 2, 200, "16:00"),
+  new Trip(2, 2, 1, 100, "15:00"),
+  new Trip(3, 3, 2, 300, "14:00"),
 ];
-
-function isSameDay(date1, date2) {
-  return (
-    date1.getFullYear() === date2.getFullYear() &&
-    date1.getDate() === date2.getDate() &&
-    date1.getMonth() === date2.getMonth()
-  );
-}
-
-function getUrlParameter(sParam) {
-  var sPageURL = window.location.search.substring(1),
-    sURLVariables = sPageURL.split("&"),
-    sParameterName,
-    i;
-
-  for (i = 0; i < sURLVariables.length; i++) {
-    sParameterName = sURLVariables[i].split("=");
-
-    if (sParameterName[0] === sParam) {
-      return typeof sParameterName[1] === undefined
-        ? true
-        : decodeURIComponent(sParameterName[1]);
-    }
-  }
-  return false;
-}
 
 // when page is loaded
 $(function () {
-    $('input.date').prop('min', function(){
-        return new Date().toJSON().split('T')[0];
-    });
-
+  $("input.date").prop("min", function () {
+    return new Date().toJSON().split("T")[0];
+  });
 
   const from = getUrlParameter("from");
   const to = getUrlParameter("to");
   const date = getUrlParameter("date");
-  
 
-  if (from || to){
-      $('input.from').val(from)
-      $('input.to').val(to)
-      $('input.date').val(date)
+  if (from || to) {
+    $("input.from").val(from);
+    $("input.to").val(to);
+    $("input.date").val(date);
   }
 
   const todayTrips = trips.filter((trip) => {
@@ -121,16 +76,18 @@ $(function () {
   }
 
   todayTrips.forEach((trip) => {
-    const route = routes.find((r) => r.id == trip.route_id);
-    const bus = buses.find((b) => b.id == trip.bus_id);
+    const route = trip.getRoute();
+    const bus = trip.getBus();
 
-    $(".result-cards").append(`<div class="result-card card shadow px-4 py-4">
+    $(".result-cards").append(`<div class="result-card card shadow px-4 py-4" data-trip="${trip.id}">
         <div class="row">
             <div class="col-md-2 text-md-left text-center">
                 <img src="./images/bus.png" alt="" class="img-fluid">
             </div>
             <div class="col-md-10">
-                <h6 class="bus-reg text-md-start text-center mt-md-0 mt-4">KCA 234J</h6>
+                <h6 class="bus-reg text-md-start text-center mt-md-0 mt-4">
+                    ${bus.reg_no}
+                </h6>
                 <div class="row align-items-center mt-md-0 mt-4 trip-details">
                     <div class="col-md-3">
                         <p><b>Route</b></p>
@@ -142,7 +99,7 @@ $(function () {
                     </div>
                     <div class="col-md-3">
                         <p><b>Availability</b></p>
-                        <p>${bus.seats - trip.booked_seats} Seats</p>
+                        <p>${trip.getAvailableSeats()} Seats</p>
                     </div>
                     <div class="col-md-2">
                         <p><b>Fare</b></p>
@@ -159,3 +116,62 @@ $(function () {
     </div>`);
   });
 });
+
+function Trip(
+  id,
+  bus_id,
+  route_id,
+  fare,
+  pickup_time,
+  travel_date = new Date(),
+  currency = "Ksh"
+) {
+  this.id = id;
+  (this.bus_id = bus_id), (this.route_id = route_id);
+  this.fare = fare;
+  this.pickup_time = pickup_time;
+  this.travel_date = travel_date;
+  this.currency = currency;
+  this.booked_seats = 0;
+}
+
+Trip.prototype.getBus = function () {
+  return buses.find((b) => b.id == this.bus_id);
+};
+
+Trip.prototype.getRoute = function () {
+  return routes.find((r) => r.id == this.route_id);
+};
+
+Trip.prototype.getAvailableSeats = function(){
+    const bus = this.getBus();
+    return bus.seats - this.booked_seats;
+}
+
+// get url parameters
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+    sURLVariables = sPageURL.split("&"),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split("=");
+
+    if (sParameterName[0] === sParam) {
+      return typeof sParameterName[1] === undefined
+        ? true
+        : decodeURIComponent(sParameterName[1]);
+    }
+  }
+  return false;
+}
+
+// check if same day
+function isSameDay(date1, date2) {
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getDate() === date2.getDate() &&
+    date1.getMonth() === date2.getMonth()
+  );
+}
